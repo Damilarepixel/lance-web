65931a3ad127f5aa79278b2a759b1195f51d5283
Dreyy00 <padigun0@gmail.com>
2020-12-02 13:07:35 +0100

    debug log

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import Facebook from './facebook.png'
+import Behance from './behance.png'
+import LinkedIn from './linkedin.png'
 
 function Links() {
     return (
-        <div>
-            <div className="footer-main">
+        <div className="footer-main">
+            <div>
                 <ul className="links">
                     <div>
-                    <p>About Us</p>
+                    <p className="footer-links">About Us</p>
                     </div>
                     <div>
-                    <p>Our Work</p>
+                    <p className="footer-links">Our Work</p>
                     </div>
                     <div>
-                    <p>Careers</p>
+                    <p className="footer-links">Careers</p>
                     </div>
                     <div>
-                    <p>Contact</p>
+                    <p className="footer-links">Contact</p>
                     </div>
                 </ul>
+            </div>
+
+            <hr />
+
+            <div className="last-item">
+                    
+                        <p className="footer-link">&#169; Lance Studio 2020</p>
+                    
             </div>
         </div>
     )
